0b8ee0fca00c5ed5e70adf0d9eac9455cdd2295d
julia <julia.pardim@icloud.com>
2026-09-18 14:36:13 -0300

    aula

---
+++ atividade1.js
@@ -0,0 +1,125 @@
+// 1- String (Textos)
+// Exercício 1 - Crie uma variável nome com seu nome e exiba no console.
+let nome = "Júlia"
+console.log(nome)
+
+// ______________________________________________________
+// Exercício 2 - Crie uma variável sobrenome e mostre nome + sobrenome.
+let sobrenome = "Pardim"
+console.log(nome + " " + sobrenome)
+// ________________________________________________________
+
+// Exercício 3 - Crie uma frase usando Template String.
+let frase = 'Olá, meu nome é ' + nome + ' ' + sobrenome + '.';
+console.log(frase)
+
+// ___________________________________________________________
+
+// Exercício 4 - Guarde o nome de uma escola e exiba.
+let nomeEscola = "SESI"
+console.log(nomeEscola)
+
+// _______________________________________________________
+// Exercício 5 - Crie duas strings e junte em uma frase.
+let str1 = "Estudando"
+let str2 = "JavaScript"
+let str3 = str1 + " " + str2
+console.log(str3)
+
+// _______________________________________________________
+
+// Exercício 6 - Exiba uma mensagem de boas-vindas.
+let boasVindas = "Seja bem-vindo(a) !"
+console.log(boasVindas)
+
+// ________________________________________________________
+// Exercício 7 - Mostre o tamanho de uma string.
+let str = "Olá, eu sou o João"
+console.log(str.length)
+
+// ________________________________________________________
+
+
+// Exercício 8 - Transforme um texto em letras maiúsculas.
+let txt = "letras maiúsculas"
+console.log(txt.toUpperCase())
+
+// _______________________________________________________
+
+// Exercício 9 - Transforme um texto em letras minúsculas.
+let txt2 = "LETRAS MINÚSCULAS"
+console.log(txt2.toLowerCase())
+console.log()
+// _______________________________________________________
+
+// Exercício 10 - Crie uma frase com nome e cidade.
+let nome2 = "João"
+let cidade = "São Paulo"
+console.log(nome + " mora em " + cidade)
+console.log()
+// _________________________________________________________
+
+// 2- Number (Números)
+// Exercício 1 - Crie dois números e exiba a soma.
+let adicao = 10 + 20
+console.log(adicao)
+console.log()
+// ____________________________________________________________
+
+// Exercício 2 - Calcule a subtração.
+let subtracao = 20 - 10
+console.log(subtracao)
+console.log()
+// ____________________________________________________________
+
+// Exercício 3 - Calcule a multiplicação.
+let multipliacacao = 10 * 2
+console.log(multipliacacao)
+console.log()
+// ______________________________________________________________
+
+// Exercício 4 - Calcule a divisão.
+let divisao = 10 / 2
+console.log(divisao)
+console.log()
+// _____________________________________________________________
+// Exercício 5 - Calcule a média de dois números.
+let media = 10 + 20
+console.log(media / 2)
+
+// _____________________________________________________________
+
+// Exercício 6 - Calcule o dobro de um número.
+let dobroNumero = 100 * 2
+console.log()
+
+// _____________________________________________________________
+
+// Exercício 7 - Calcule o resto da divisão.
+let dividendo = 15
+let divisor = 4
+let resto = dividendo % divisor;
+console.log(resto)
+console.log()
+// _____________________________________________________________
+
+
+// Exercício 8 - Crie um preço e um desconto.
+let precoOriginal = 100.00
+let descontoPercentual = 15
+let precoFinal = precoOriginal * (1 - descontoPercentual / 100);
+console.log(precoFinal)
+console.log()
+// _____________________________________________________________
+// Exercício 9 - Converta idade em meses.
+let idadeAnos = 25 * 12
+console.log(idadeAnos)
+console.log()
+// ____________________________________________________________
+
+// Exercício 10 - Calcule o quadrado de um número.3
+let quadrado = 6 ** 2
+console.log(quadrado)
+console.log()
+// _____________________________________________________________
+
